df7381a3db2f2f129624ed173af7f51aa25f5a0c
uladbohdan <vlbohdan@gmail.com>
2017-09-14 23:58:34 -0700

    all articles come from DB

--- article.go
+++ article.go
@@ -26,23 +26,27 @@ type ArticlePage struct {
 
 func handleArticle(w http.ResponseWriter, r *http.Request) {
 	ss := strings.Split(r.URL.Path, "/")
-	// aid, _ := strconv.Atoi(ss[2])
-	var article Article
-	has, err := db.Where("i_d = ?", ss[2]).Get(&article)
-	if err != nil {
-		fmt.Println("article error: ", err)
-	}
-	if !has {
-		fmt.Println("article with id ", ss[2], "not found")
-	}
+	article := getArticleFromDB(ss[2])
 
 	articleWrapper := ArticlePage{
 		Article:  article,
-		Featured: []Article{articles[1], articles[2], articles[3]},
+		Featured: []Article{getArticleFromDB("1"), getArticleFromDB("2"), getArticleFromDB("3")},
 	}
 
 	fmt.Println("requesting article", ss[2])
 	tmpl, _ := template.ParseFiles(templateList("article/base", "navbar", "footer", "article/featured-tile", "header")...)
 
 	tmpl.Execute(w, articleWrapper)
+}
+
+func getArticleFromDB(id string) Article {
+	var article Article
+	has, err := db.Where("i_d = ?", id).Get(&article)
+	if err != nil {
+		fmt.Println("article error:", err)
+	}
+	if !has {
+		fmt.Println("article with id ", id, "not found")
+	}
+	return article
 }
--- index.go
+++ index.go
@@ -14,7 +14,7 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(templateList("index/index", "navbar", "footer", "index/regular-tile", "header")...)
 
 	indexPage := &IndexPage{
-		Top3: []Article{articles[1], articles[2], articles[3]},
+		Top3: []Article{getArticleFromDB("1"), getArticleFromDB("2"), getArticleFromDB("3")},
 	}
 
 	tmpl.Execute(w, indexPage)
